84ea96c355a41169da4ee87e4141400eaffafe78
Vladimir Zakharov <enough.tea@outlook.com>
2017-01-16 00:03:01 +0300

    Added GitVersion.yml for clarity.

--- Src/Essentions/Properties/AssemblyInfo.cs
+++ Src/Essentions/Properties/AssemblyInfo.cs
@@ -16,4 +16,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyFileVersion("1.0.1.0")]
 [assembly: CLSCompliant(true)]
 [assembly: InternalsVisibleTo("Essentions.Tests")]
-[assembly: AssemblyInformationalVersion("1.0.1+5.Branch.master.Sha.0ca2585f2063577e90655e982434fffef86c716e")]
+[assembly: AssemblyInformationalVersion("1.0.1+Branch.master.Sha.6046817467bb566606791d254a9e757e00f93623")]
